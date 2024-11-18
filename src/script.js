ebee1e913b894abdebe2ad0d4a5e81d4be8ec180
Artem <postaluka07@gmail.com>
2024-11-18 17:33:52 +0200

    add 01,02,03,04,08,11,12

--- src/script.js
+++ src/script.js
@@ -5,11 +5,14 @@ import * as rive from "@rive-app/canvas"
  */
 const canvas01beforeProvisioning = document.getElementById('canvas01beforeProvisioning')
 const canvas02afterProvisioning = document.getElementById('canvas02afterProvisioning')
+const canvas03bluetoothPairing = document.getElementById('canvas03bluetoothPairing')
+const canvas04bluetoothConnected = document.getElementById('canvas04bluetoothConnected')
+const canvas08firmwareUpdate = document.getElementById('canvas08firmwareUpdate')
+const canvas11beforeProvisioning = document.getElementById('canvas11beforeProvisioning')
+const canvas12afterProvisioning = document.getElementById('canvas12afterProvisioning')
 
 
 //canvas01beforeProvisioning
-
-
 
 const inito01beforeProvisioning = new rive.Rive(
     {
@@ -28,11 +31,7 @@ const inito01beforeProvisioning = new rive.Rive(
     }
 )
 
-
-
 //canvas02afterProvisioning
-
-
 
 const inito02afterProvisioning = new rive.Rive(
     {
@@ -40,6 +39,101 @@ const inito02afterProvisioning = new rive.Rive(
         canvas: canvas02afterProvisioning,
         autoplay: true,
         stateMachines: 'INITO SM 02 Device wake up via button - after provisioning',
+        // animations: ['EYES', 'CABIN MOVEMENT', 'WHEEL CTRL', 'FRONT GROUP', 'WHEEL HOVER exit', 'WHEEL HOVER enter', 'SIGNAL'],
+        // onLoad: (_) =>
+        // {
+        //     const stateMachineInputs = huppDriver.stateMachineInputs('CAR STATE MACHINE')
+        //     // trigger = stateMachineInputs[0]
+        //     console.log(stateMachineInputs);
+
+        // },
+    }
+)
+
+//canvas03bluetoothPairing
+
+const inito03bluetoothPairing = new rive.Rive(
+    {
+        src: 'rive/inito_.riv',
+        canvas: canvas03bluetoothPairing,
+        autoplay: true,
+        stateMachines: 'INITO SM 03 Bluetooth pairing',
+        // animations: ['EYES', 'CABIN MOVEMENT', 'WHEEL CTRL', 'FRONT GROUP', 'WHEEL HOVER exit', 'WHEEL HOVER enter', 'SIGNAL'],
+        // onLoad: (_) =>
+        // {
+        //     const stateMachineInputs = huppDriver.stateMachineInputs('CAR STATE MACHINE')
+        //     // trigger = stateMachineInputs[0]
+        //     console.log(stateMachineInputs);
+
+        // },
+    }
+)
+
+//canvas04bluetoothConnected
+
+const inito04bluetoothConnected = new rive.Rive(
+    {
+        src: 'rive/inito_.riv',
+        canvas: canvas04bluetoothConnected,
+        autoplay: true,
+        stateMachines: 'INITO SM 04 Bluetooth connected',
+        // animations: ['EYES', 'CABIN MOVEMENT', 'WHEEL CTRL', 'FRONT GROUP', 'WHEEL HOVER exit', 'WHEEL HOVER enter', 'SIGNAL'],
+        // onLoad: (_) =>
+        // {
+        //     const stateMachineInputs = huppDriver.stateMachineInputs('CAR STATE MACHINE')
+        //     // trigger = stateMachineInputs[0]
+        //     console.log(stateMachineInputs);
+
+        // },
+    }
+)
+
+//canvas08firmwareUpdate
+
+const inito08firmwareUpdate = new rive.Rive(
+    {
+        src: 'rive/inito_.riv',
+        canvas: canvas08firmwareUpdate,
+        autoplay: true,
+        stateMachines: 'INITO SM 08 Firmware update',
+        // animations: ['EYES', 'CABIN MOVEMENT', 'WHEEL CTRL', 'FRONT GROUP', 'WHEEL HOVER exit', 'WHEEL HOVER enter', 'SIGNAL'],
+        // onLoad: (_) =>
+        // {
+        //     const stateMachineInputs = huppDriver.stateMachineInputs('CAR STATE MACHINE')
+        //     // trigger = stateMachineInputs[0]
+        //     console.log(stateMachineInputs);
+
+        // },
+    }
+)
+
+//canvas11beforeProvisioning
+
+const inito11beforeProvisioning = new rive.Rive(
+    {
+        src: 'rive/inito_.riv',
+        canvas: canvas11beforeProvisioning,
+        autoplay: true,
+        stateMachines: 'INITO SM 11 Turning on the device - before provisioning',
+        // animations: ['EYES', 'CABIN MOVEMENT', 'WHEEL CTRL', 'FRONT GROUP', 'WHEEL HOVER exit', 'WHEEL HOVER enter', 'SIGNAL'],
+        // onLoad: (_) =>
+        // {
+        //     const stateMachineInputs = huppDriver.stateMachineInputs('CAR STATE MACHINE')
+        //     // trigger = stateMachineInputs[0]
+        //     console.log(stateMachineInputs);
+
+        // },
+    }
+)
+
+//canvas12afterProvisioning
+
+const inito12afterProvisioning = new rive.Rive(
+    {
+        src: 'rive/inito_.riv',
+        canvas: canvas12afterProvisioning,
+        autoplay: true,
+        stateMachines: 'INITO SM 12 Turning on the device - after provisioning',
         // animations: ['EYES', 'CABIN MOVEMENT', 'WHEEL CTRL', 'FRONT GROUP', 'WHEEL HOVER exit', 'WHEEL HOVER enter', 'SIGNAL'],
         // onLoad: (_) =>
         // {
